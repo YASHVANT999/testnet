619640223eb7c4657dabb27cbb9fb00b2c9fe5d9
YASHVANT999 <mlp.yashvantgupta@gmail.com>
2023-06-23 22:34:45 +0530

    push

--- money_splitter/settings.py
+++ money_splitter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'daycb3@9#i^2a!!$=%54vrve8ilqsh9i4n_xtzdew#g9_*l7e='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -151,5 +151,5 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
